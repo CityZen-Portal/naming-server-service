73735a76ca977d5e73bb903dec96df540a12e13b
Siranjeevi619 <siranjeevi0619@gmail.com>
2025-07-27 14:33:25 +0530

    feat:added eruka annotation

--- src/main/java/com/cityzen/namingserver/NamingserverApplication.java
+++ src/main/java/com/cityzen/namingserver/NamingserverApplication.java
@@ -2,9 +2,10 @@ package com.cityzen.namingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
-
+@EnableEurekaServer
 @SpringBootApplication
 public class NamingserverApplication {
 
